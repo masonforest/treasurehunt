e5988b8c065ab2fae653973dc805929dab691f0e
Seamus Cantillon <seamus@marinosoftware.com>
2016-11-05 10:03:45 +0000

    inital AR commit

---
+++ iOS/TreasureHunt/TreasureHunt/ARViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ARViewController.swift
+//  TreasureHunt
+//
+//  Created by Seamus on 05/11/2016.
+//  Copyright © 2016 Marinosoftware. All rights reserved.
+//
+
+import UIKit
+
+class ARViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

5d46e6f41bd7f3aad916fc121b24683eb6a7bbbc
Tim Colla <tim.colla@marinosoftware.com>
2016-11-05 09:26:02 +0000

    Quick setup bare iOS project

---
+++ iOS/TreasureHunt/TreasureHunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TreasureHunt
+//
+//  Created by Tim Colla on 04/11/2016.
+//  Copyright © 2016 Marinosoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

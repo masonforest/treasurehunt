703c7cbeaed0bdb7a8047c44f8226e0c0ef54130
Mason Fischer <mason@thoughtbot.com>
2016-11-06 08:48:58 +0000

    Add /reset API

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ var _ = require('lodash');
 var SolidityFunction = require('web3/lib/web3/function');
 var web3 = new Web3();
 var app = express()
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+web3.setProvider(new web3.providers.HttpProvider('https://frontier-lb.ether.camp'));
 
 // Live
 const ACCOUNT_ADDRESS = "0x06b50b1fee1a46d8803d017e6bf363a3f904d8fe";
 const ACCOUNT_KEY = "f90f1c84ffdd40d660ce56392e6b9427bd7922004747980c6841b419b0384641";
 
-const CONTRACT_ADDRESS = "0x6026b764795e29f18304bbe82c7d5ed305dbc41e";
+const CONTRACT_ADDRESS = "0x31111a62437f75a82405564a946637b07fb208bb";
 treasureHuntAbi =
-[{"constant":true,"inputs":[{"name":"player","type":"address"}],"name":"GetNextHintForPlayer","outputs":[{"name":"nextHint","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"latitude","type":"int256"},{"name":"longitude","type":"int256"},{"name":"player","type":"address"}],"name":"CheckAnswerForPlayer","outputs":[{"name":"correct","type":"bool"},{"name":"nextHint","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"latitude","type":"int256"},{"name":"longitude","type":"int256"}],"name":"SubmitAnswer","outputs":[],"payable":false,"type":"function"},{"inputs":[],"type":"constructor"}]
+[{"constant":true,"inputs":[{"name":"player","type":"address"}],"name":"GetNextHintForPlayer","outputs":[{"name":"nextHint","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"latitude","type":"int256"},{"name":"longitude","type":"int256"},{"name":"player","type":"address"}],"name":"CheckAnswerForPlayer","outputs":[{"name":"correct","type":"bool"},{"name":"nextHint","type":"string"},{"name":"nextVideo","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"latitude","type":"int256"},{"name":"longitude","type":"int256"}],"name":"SubmitAnswer","outputs":[{"name":"nextHint","type":"string"},{"name":"nextVideo","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"player","type":"address"}],"name":"GetNextVideoForPlayer","outputs":[{"name":"nextVideo","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"player","type":"address"}],"name":"ResetPlayer","outputs":[{"name":"nextHint","type":"string"},{"name":"nextVideo","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"player","type":"address"}],"name":"GetNextHintAndVideoForPlayer","outputs":[{"name":"nextHint","type":"string"},{"name":"nextVideo","type":"string"}],"payable":false,"type":"function"},{"inputs":[],"type":"constructor"}]
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -28,8 +28,8 @@ treasureHunt = TreasureHunt.at(CONTRACT_ADDRESS);
 
 app.get('/nextHint', function (req, res) {
   res.send(JSON.stringify({
-    hint: treasureHunt.GetNextHintForPlayer(ACCOUNT_ADDRESS)
-    video: "video1",
+    hint: treasureHunt.GetNextHintForPlayer(ACCOUNT_ADDRESS),
+    video: treasureHunt.GetNextVideoForPlayer(ACCOUNT_ADDRESS)
   }));
 })
 
@@ -39,29 +39,25 @@ app.get('/balance', function (req, res) {
   })
 })
 
+app.post('/reset', function (req, res) {
+  callEtherumFunction('ResetPlayer', ACCOUNT_ADDRESS);
+
+  res.send();
+})
+
 app.post('/', function (req, res) {
   callEtherumFunction('SubmitAnswer',
     parseInt(req.body.latitude),
     parseInt(req.body.longitude));
 
-  console.log({
-    latitiude: req.body.latitude,
-    longitude: req.body.longitude,
-    success: true
-  });
-  res.send(JSON.stringify({
-    hint: "Got to the first floor",
-    video: "video1",
-    success: true
-  }));
+  res.send();
 })
 
 function callEtherumFunction(functionName, ...functionArgs) {
-  console.log(functionArgs);
+console.log(_.find(treasureHuntAbi,{ name: functionName }))
   var solidityFunction = new SolidityFunction('', _.find(treasureHuntAbi, { name: functionName }), '');
   var payloadData = solidityFunction.toPayload(functionArgs).data;
   gasPrice = web3.eth.gasPrice;
-  console.log(gasPrice)
   gasPriceHex = web3.toHex(gasPrice.times(3));
   gasLimitHex = web3.toHex(300000);
   nonce =  web3.eth.getTransactionCount(ACCOUNT_ADDRESS) ;
@@ -75,6 +71,8 @@ function callEtherumFunction(functionName, ...functionArgs) {
       value: '0x00',
       data: payloadData
   };
+
+console.log(rawTx)
   var tx = new Tx(rawTx);
   tx.sign(new Buffer(ACCOUNT_KEY, 'hex'));
   var serializedTx = tx.serialize();
